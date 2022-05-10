6d082461b080ca74b6d48b720ef1f1f1be49112d
Shax <nedcas69@gmail.com>
2022-05-10 10:26:10 +0500

    main

--- lib/book.rb
+++ lib/book.rb
@@ -6,20 +6,20 @@ class Book < Product
     end
    
     def arr
-        @array_book = {names: @names, price: @price, amount: @amount, genre: @genre, author: @author}
+        @array_book = [{names: @names, price: @price, amount: @amount, genre: @genre, author: @author}]
     end
     
     def to_strings
-        # @array_book.each_with_index do |arrays, number|
+        @array_book.each_with_index do |keys, number|
         #     puts "Книга \" #{arrays[:names].to_s} \", #{arrays[:genre].to_s}, автор - #{arrays[:author].to_s},
         #      #{arrays[:price].to_s} руб. (осталось - #{arrays[:amount].to_s}) "
         # end
-        @array_book.each_pair {|key, value| 
-            puts "#{:names} => #{@names}"
-            puts "#{:genre} => #{@genre}"
-            puts "#{:author} => #{@author}"
-            puts "#{:price} => #{@price}"
-            puts "#{:amount} => #{@amount}"
-        }
+
+            puts "Название Книги => #{keys[:names]}"
+            puts "Жанр => #{keys[:genre]}"
+            puts "Автор => #{keys[:author]}"
+            puts "Цена => #{keys[:price]}"
+            puts "Количества => #{keys[:amount]}"
+        end
     end
 end
--- lib/movie.rb
+++ lib/movie.rb
@@ -5,18 +5,18 @@ class Movie < Product
         super
     end
     def arr
-        @array_movie = {names: @names, price: @price, amount: @amount, years: @years, rejissor: @rejissor}
+        @array_movie = [{names: @names, price: @price, amount: @amount, years: @years, rejissor: @rejissor}]
     end
     
     def to_strings
-        @array_movie.each_pair {|key, value| 
-            puts "#{:names} => #{@names}"
-            puts "#{:years} => #{@years}"
-            puts "#{:rejissor} => #{@rejissor}"
-            puts "#{:price} => #{@price}"
-            puts "#{:amount} => #{@amount}"
+        @array_movie.each_with_index do |keys, number|
+            puts "Название фильма => #{keys[:names]}"
+            puts "Год выпуска => #{keys[:years]}"
+            puts "Режиссер => #{keys[:rejissor]}"
+            puts "Цена => #{keys[:price]}"
+            puts "Количества => #{keys[:amount]}"
+        end
         
-        }
         # @array_movie.each_with_index do |array_movie, number|
         #     puts "Фильм \" #{array_movie[:names].to_s} \", #{array_movie[:years].to_s} год, 
         #     \n\rРежиссёр - #{array_movie[:rejissor].to_s}, #{array_movie[:price].to_s} руб. 
